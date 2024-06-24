699d958c444afffd5603b3b7d37b4e202a8bb3fe
tillo <tillo4079@gmail.com>
2024-06-25 03:29:13 +0500

    6 ta calculate funktsiya qoshildi

--- main.dart
+++ main.dart
@@ -2,4 +2,11 @@ import 'index.dart';
 
 void main() {
   calculate(12, 32);
+  calculate(12, 32);
+  calculate(12, 32);
+  calculate(12, 32);
+  calculate(12, 32);
+  calculate(12, 32);
+  calculate(12, 32);
+  print("object");
 }
