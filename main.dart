781f4f7502ca9793dd4eb63b4b3d732ae55e6256
tillo <tillo4079@gmail.com>
2024-06-25 03:33:38 +0500

    misol

--- main.dart
+++ main.dart
@@ -2,11 +2,5 @@ import 'index.dart';
 
 void main() {
   calculate(12, 32);
-  calculate(12, 32);
-  calculate(12, 32);
-  calculate(12, 32);
-  calculate(12, 32);
-  calculate(12, 32);
-  calculate(12, 32);
   print("object");
 }
